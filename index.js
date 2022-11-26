9f9db25b54b15411fc3b985cd71e540d5d9eba43
Mahmod-Hassan <mahmodhasan7788@gmail.com>
2022-11-26 06:29:42 +0600

    create post method for send booking to server

--- index.js
+++ index.js
@@ -12,10 +12,35 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcblope.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
 
+    // All Collection //
+    const categoryCollection = client.db('resaleProducts').collection('categories');
+    const productCollection = client.db('resaleProducts').collection('products');
+    const bookingCollection = client.db('resaleProducts').collection('bookings');
+
+    app.get('/categories', async (req, res) => {
+        const query = {};
+        const result = await categoryCollection.find(query).toArray();
+        res.send(result);
+    })
+    app.get('/category/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { category_id: id };
+        const result = await productCollection.find(query).toArray();
+        res.send(result);
+    })
+    app.post('/bookings', async (req, res) => {
+        const booking = req.body;
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result);
+    })
+
+
+}
+run().catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('server is running')
